f6c85d88d8baa057a1c4fc27c620ce9d3d276cc1
ChrisDezenLauro-UCM <chriszenlauro@gmail.com>
2025-08-29 18:41:53 +0800

    This is my first Debugging practice.
    I added a documentation of the different bugs I found.

---
+++ Chapter_1/FixDebug.java
@@ -0,0 +1,14 @@
+public class FixDebug {
+
+public static void main(String[] args) { 
+    System.out.println("Hello World!"); } 
+}
+/** These are the bugs I found Sir Kim
+Bugs:
+1. The class name can’t have spaces since Java doesn’t allow multiple separate words as identifiers.  
+2. The method should be written as "main" (all lowercase), not "Main".  
+3. The String arguments need square brackets [] for the array.  
+4. “System” is misspelled — the letter “e” is missing.  
+5. A semicolon is required at the end of the print statement.  
+6. The class is missing a closing curly brace at the end.  
+*/
