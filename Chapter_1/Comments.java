3f8b236cb3cc020b6d18e33686374a09d3d442de
LauroChrisDezenUCM <chriszenlauro@gmail.com>
2025-08-29 18:27:39 +0800

    The other Exercises

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,20 @@
+public class Comments {
+    // This is called a single line comment by using a double slash then adding ur comment after
+
+
+    
+    /* Called
+     * is
+     * a
+     * Multi-line
+     * comment
+     * it is pretty self explanatory hehe
+     :3 */
+
+
+
+     /** This is apparently called a Doc Comment/Documentation comment from what I searched it gives you the same as
+      * as multi-lined comments but is used for emphasizing or add context to a code or code block you want to explain to
+      the user and is cooler because it starts with two asterisks B)
+      */
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,10 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+System.out.println("Black hole sun by Soundgarden");
+System.out.println("Steal the warm wind, tired friend");
+System.out.println("Times are gone");
+System.out.println("For honest men");
+System.out.println("Sometimes, far too long for snakes");
+        }
+    }
+
